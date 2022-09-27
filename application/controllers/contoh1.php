4f71aa791f92833b6244f5a347d4fe771ebdb28d
Agusmartinmanik <martinmanik1998@gmail.com>
2022-09-27 20:06:26 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1";
+        echo "<h1>Nama saya Agus Martin Manik
+        saya tinggal di daerah Bekasi timur
+        olah raga yang saya sukai adalah 
+        Sepak Bola atau Futsal</h1";
+    }
+}    
---
+++ application/views/view-latihan.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <tittle>Latihan</title>
+</head>
+
+<body>
+    halo kawan.. Yuk kita belajar web programming... !!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+    $nilai1. " + " .$nilai2 ." = " .$hasil; ?>
+</body>    
+</html>
